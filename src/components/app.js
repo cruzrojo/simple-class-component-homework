5c6e08bfc05f410fa047940dfd98d66b078abdec
cruzrojo <burkerich@gmail.com>
2020-02-20 17:17:34 -0700

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Burke Rich Homework Assignment</h1>
+        <h2>Awesome Power</h2>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
