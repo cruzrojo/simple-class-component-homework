4ec12075c3c18322198cba6172d2563022acacbc
cruzrojo <burkerich@gmail.com>
2020-02-20 19:31:41 -0700

    added a navigation.js and added the content to the file. THen added button versions of that to the file.

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import moment from "moment";
 
+import NavigationContainer from "./navigation";
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Burke Rich Homework Assignment</h1>
         <h2>Awesome Power</h2>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <NavigationContainer />
         <h1>Hello World</h1>
       </div>
     );
---
+++ src/components/navigation.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class NavigationContainer extends Component {
+    render() {
+        return (
+            <div className="nav-wrapper">
+                <div>
+                    <button>Home</button>
+                    Home
+                </div>
+                <div>
+                    <button>About</button>
+                    About
+                </div>
+                <div>
+                    <button>Contact</button>
+                    Contact
+                </div>
+            </div>
+        )
+    }
+}
