f1dd6ffc6d3f0c0c66f10013f17b276e1bb19ccc
cruzrojo <burkerich@gmail.com>
2020-02-22 09:12:15 -0700

    making a commit of the recent changes

--- src/components/app.js
+++ src/components/app.js
@@ -6,10 +6,10 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Burke Rich Homework Assignment</h1>
         <h2>Awesome Power</h2>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-        <NavigationContainer />
         <h1>Hello World</h1>
       </div>
     );
